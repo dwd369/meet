4046a02c661fda2b77f0bb50dfa9bafc8e833f9e
dwd369 <dwd7290@gmail.com>
2023-12-03 00:53:11 -0800

    add InfoAlert and ErrorAlert to app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NumberOfEvents from './components/NumberOfEvents';
 
 import { useState, useEffect } from 'react';
 import { extractLocations, getEvents } from './api';
+import { InfoAlert, ErrorAlert } from './components/Alert';
 
 import './App.css';
 
@@ -14,6 +15,8 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+  const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -31,8 +34,19 @@ const App = () => {
 
   return (
     <div className="App">
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity}/>
-      <NumberOfEvents setCurrentNOE={setCurrentNOE}/>
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+      </div>
+      <CitySearch
+        allLocations={allLocations}
+        setCurrentCity={setCurrentCity}
+        setInfoAlert={setInfoAlert}
+        />
+      <NumberOfEvents
+        setCurrentNOE={setCurrentNOE}
+        setErrorAlert={setErrorAlert}
+        />
       <EventList events={events}/>
     </div>
   )
--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,10 +1,26 @@
 import { useState } from "react";
 
-const NumberOfEvents = ( { setCurrentNOE}) => {
+const NumberOfEvents = ( { setCurrentNOE, setErrorAlert}) => {
 
     const handleInputChanged = (event) => {
         const value = event.target.value;
-        setCurrentNOE(value);
+        let errorText;
+
+        if (Number.isInteger(parseInt(value)) && parseInt(value) <= 32 && parseInt(value) > 0) {
+            errorText = "";
+            setCurrentNOE(value);
+        } else if (!value) {
+            errorText = "";
+            setCurrentNOE(32);
+        } else if (parseInt(value) < 0) {
+            errorText = "Only positive numbers are allowed"
+        } else if (parseInt(value) > 32) {
+            errorText = "Enter the number between 1 and 32"
+        } else {
+            errorText = "Invalid input. Please enter a valid a number"
+        }
+
+        setErrorAlert(errorText);
 
     }
 
