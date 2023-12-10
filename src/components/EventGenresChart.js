556356ee4546239c8763e69fdf633935b0495324
dwd369 <dwd7290@gmail.com>
2023-12-09 22:53:40 -0800

    clean up code in EventGeneresChart.js

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -69,21 +69,6 @@ const EventGenresChart = ({events}) => {
             </Pie>
 
             <Legend />
-            
-        {/* <Pie
-            data={data}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            label = {renderCustomizedLabel}
-            outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
-        >
-              {data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-              ))}
-            </Pie> */}
           </PieChart>
         </ResponsiveContainer>
     )
